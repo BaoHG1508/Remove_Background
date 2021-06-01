9727574614a0b5533f44a0f5730713eb648e9f84
Nanneless <54936754+Nanneless@users.noreply.github.com>
2021-06-01 17:55:09 +0700

    Video feature + Fixed some bug

--- GUI.py
+++ GUI.py
@@ -8,21 +8,6 @@ import numpy as np
 import cv2
 import imageio
 
-class StoppableThread(threading.Thread):
-    """Thread class with a stop() method. The thread itself has to check
-    regularly for the stopped() condition."""
-
-    def __init__(self,  *args, **kwargs):
-        super(StoppableThread, self).__init__(*args, **kwargs)
-        self._stop_event = threading.Event()
-
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
-
-
 def ImportBackground():
     #import back_ground
     global back_ground
@@ -126,7 +111,7 @@ def Change_Background(label,import_button,import_video):
         label.config(image=img)
         label.image = img
     elif video != []:
-        thread = StoppableThread(target=stream_bg_changed, args=(label,))
+        thread = threading.Thread(target=stream_bg_changed, args=(label,))
         thread.daemon = 1
         thread.start()
 
